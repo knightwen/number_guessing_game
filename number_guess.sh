4883382f8cad92e4968e5e3a36cb82e8974f9877
knightwen <92281249+knightwen@users.noreply.github.com>
2025-08-05 11:08:34 -0400

    feat: day1 complete

--- number_guess.sh
+++ number_guess.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+PSQL="psql -U postgres -d number_guess -t --no-align -c"
+
 echo -e "\nEnter your username:"
 read username
 
 # check whether used before
-# if so
-if [[ ]]; then
-echo "Welcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses."
+user_info=$($PSQL "SELECT*FROM number_of_game,number_of_guess WHERE user_name='$username';")
+# if so, welcome and update data.
+if [[ -z $user_info ]]; then
+echo "Welcome, $username! It looks like this is your first time here."
+$PSQL "INSERT INTO user_stories(user_name) VALUES('$username');"
+# otherwise
 else
-echo "Welcome, <username>! It looks like this is your first time here."
+"Welcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses."
+echo 
 fi
 
 echo "Guess the secret number between 1 and 1000:"
