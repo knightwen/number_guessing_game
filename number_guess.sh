09d4f213dd8dcf9bc15b6f6e9c6d6b3aa5759d3f
knightwen <92281249+knightwen@users.noreply.github.com>
2025-08-06 08:02:20 -0400

    feat: final test pass

--- number_guess.sh
+++ number_guess.sh
@@ -13,11 +13,9 @@ if [[ -z $user_info ]]; then
   insert_name=($PSQL "INSERT INTO users(user_name, number_of_game) VALUES('$username', 1);")
 # otherwise
 else
-  echo "$user_info" | while IFS="|" read TOTAL_GAME NUMBER_GUESS
-  do
-    echo "Welcome back, $username! You have played $TOTAL_GAME games, and your best game took $NUMBER_GUESS guesses."
-    insert_totalGame=$($PSQL "INSERT INTO users(number_of_game) VALUES(number_of_game+1);")
-  done
+  IFS="|" read TOTAL_GAME NUMBER_GUESS <<< "$user_info"
+  echo "Welcome back, $username! You have played $TOTAL_GAME games, and your best game took $NUMBER_GUESS guesses."
+  update_user=$($PSQL "UPDATE users SET number_of_game = number_of_game + 1 WHERE user_name = '$username';")
 fi
 
 result=$(( RANDOM % 1000 + 1 ))
@@ -27,15 +25,17 @@ echo "Guess the secret number between 1 and 1000:"
 # check data type
 while true; do
   read guess_number
-  if [[ $guess_number =~ ^[0-9]+$ ]]; then
+  if ! [[ $guess_number =~ ^[0-9]+$ ]]; then
     echo "That is not an integer, guess again:"
+    continue
   fi
 
-  (($guess_count++))
+  (( guess_count++ ))
 
   if [[ $guess_number == $result ]]; then
     echo "You guessed it in $guess_count tries. The secret number was $result. Nice job!"
-    # insert into database
+    # update database
+    $PSQL "UPDATE users SET number_of_guess=$guess_count WHERE user_name='$username' AND (number_of_guess IS NULL OR number_of_guess > $guess_number);"
     break
   elif [[ $guess_number > $result ]]; then
     echo "It's higher than that, guess again:"
