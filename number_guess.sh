5b95296b9ba5bd00c34a840eee8d8ac5fb4b5ab2
knightwen <92281249+knightwen@users.noreply.github.com>
2025-08-06 08:32:11 -0400

    feat: final version

--- number_guess.sh
+++ number_guess.sh
@@ -5,39 +5,40 @@ PSQL="psql -U postgres -d number_guess -t --no-align -c"
 echo -e "\nEnter your username:"
 read username
 
-# check whether used before
-user_info=$($PSQL "SELECT number_of_game,number_of_guess FROM users WHERE user_name='$username';")
-# if so, welcome and update data.
-if [[ -z $user_info ]]; then
-  echo "Welcome, $username! It looks like this is your first time here."
-  insert_name=($PSQL "INSERT INTO users(user_name, number_of_game) VALUES('$username', 1);")
-# otherwise
+# check whether user exists
+user_info=$($PSQL "SELECT games_played, best_game FROM users WHERE user_name='$username';")
+
+if [[ -n $user_info ]]; then
+  IFS="|" read GAMES_PLAYED BEST_GAME <<< "$user_info"
+  echo "Welcome back, $username! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  UPDATE=$($PSQL "UPDATE users SET games_played = games_played + 1 WHERE user_name = '$username';")
 else
-  IFS="|" read TOTAL_GAME NUMBER_GUESS <<< "$user_info"
-  echo "Welcome back, $username! You have played $TOTAL_GAME games, and your best game took $NUMBER_GUESS guesses."
-  update_user=$($PSQL "UPDATE users SET number_of_game = number_of_game + 1 WHERE user_name = '$username';")
+  echo "Welcome, $username! It looks like this is your first time here."
+  INSERT=$($PSQL "INSERT INTO users(user_name, games_played) VALUES('$username', 1);")
 fi
 
-result=$(( RANDOM % 1000 + 1 ))
-guess_count=0;
+# game starts
+secret_number=$(( RANDOM % 1000 + 1 ))
+number_of_guesses=0
 
 echo "Guess the secret number between 1 and 1000:"
-# check data type
+
 while true; do
-  read guess_number
-  if ! [[ $guess_number =~ ^[0-9]+$ ]]; then
+  read guess
+  if ! [[ $guess =~ ^[0-9]+$ ]]; then
     echo "That is not an integer, guess again:"
     continue
   fi
 
-  (( guess_count++ ))
+  (( number_of_guesses++ ))
 
-  if [[ $guess_number == $result ]]; then
-    echo "You guessed it in $guess_count tries. The secret number was $result. Nice job!"
-    # update database
-    $PSQL "UPDATE users SET number_of_guess=$guess_count WHERE user_name='$username' AND (number_of_guess IS NULL OR number_of_guess > $guess_number);"
+  if [[ $guess -eq $secret_number ]]; then
+    echo "You guessed it in $number_of_guesses tries. The secret number was $secret_number. Nice job!"
+
+    # update best_game if better or not set
+    UPDATE=$($PSQL "UPDATE users SET best_game = $number_of_guesses WHERE user_name='$username' AND (best_game IS NULL OR best_game > $number_of_guesses);")
     break
-  elif [[ $guess_number > $result ]]; then
+  elif [[ $guess -gt $secret_number ]]; then
     echo "It's higher than that, guess again:"
   else
     echo "It's lower than that, guess again:"
